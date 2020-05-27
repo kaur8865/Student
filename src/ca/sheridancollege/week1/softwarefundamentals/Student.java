ea4a25595a596816a7049b310a5b4c71e5af1f38
Ravneet <Ravneet@DESKTOP-9E0LTGB>
2020-05-27 15:48:19 -0400

    PartTime Commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -12,6 +12,26 @@ package ca.sheridancollege.week1.softwarefundamentals;
 public class Student {
     
     private String name;
+    private String numOfHours;
+
+    /**
+     * Get the value of numOfHours
+     *
+     * @return the value of numOfHours
+     */
+    public String getNumOfHours() {
+        return numOfHours;
+    }
+
+    /**
+     * Set the value of numOfHours
+     *
+     * @param numOfHours new value of numOfHours
+     */
+    public void setNumOfHours(String numOfHours) {
+        this.numOfHours = numOfHours;
+    }
+
 
     /**
      * Get the value of name
